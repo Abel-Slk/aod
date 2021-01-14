c3ea1b950842992a1b40bb84f78e83fa8c607d0a
grgp <grgp722@gmail.com>
2021-01-14 20:15:15 +0300

    header and main screen

---
+++ #source/js/script.js
@@ -0,0 +1,28 @@
+
+$('.wrapper').addClass('loaded');
+
+
+let iconMenu = document.querySelector(".icon-menu");
+let body = document.querySelector("body");
+let menuBody = document.querySelector(".menu__body");
+if (iconMenu) {
+	iconMenu.addEventListener("click", function () {
+		iconMenu.classList.toggle("active");
+		menuBody.classList.toggle("active");
+		body.classList.toggle("lock");
+	});
+}
+
+
+function ibg() {
+	if (isIE()) {
+		let ibg = document.querySelectorAll(".ibg");
+		for (var i = 0; i < ibg.length; i++) {
+			if (ibg[i].querySelector('img') && ibg[i].querySelector('img').getAttribute('src') != null) {
+				ibg[i].style.backgroundImage = 'url(' + ibg[i].querySelector('img').getAttribute('src') + ')';
+			}
+		}
+	}
+}
+ibg();
+
